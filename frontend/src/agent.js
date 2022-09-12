42448e49ef175435fccc19c24a3d82eac7707796
SammyShehter <sammyshehter@gmail.com>
2022-09-12 11:52:59 +0300

    added env var for frontend

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -6,7 +6,7 @@ const superagent = superagentPromise(_superagent, global.Promise);
 const BACKEND_URL =
   process.env.NODE_ENV !== "production"
     ? process.env.REACT_APP_BACKEND_URL
-    : "https://api.anythink.market";
+    : process.env.BACKEND_URL;
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
